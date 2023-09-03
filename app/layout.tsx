936caf418dc1f5d6dab36c433cc1e203be99d6c3
Manh Khiem <manhkhiem2703@gmail.com>
2023-09-03 15:24:38 +0700

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import { Footer,NavBar } from "@/components";
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata = {
+  title: "Car Hub",
+  description: "Discover world's best car showcase application",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <NavBar />
+        {children}
+        <Footer />{" "}
+      </body>
+    </html>
+  );
+}
---
+++ components/index.ts
@@ -0,0 +1,22 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CardDetail from './CarDetails';
+import ShowMore from "./ShowMore";
+export {
+    Hero,
+    CustomButton,
+    NavBar,
+    Footer,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer,
+    CarCard,
+    CardDetail,
+    ShowMore
+}
